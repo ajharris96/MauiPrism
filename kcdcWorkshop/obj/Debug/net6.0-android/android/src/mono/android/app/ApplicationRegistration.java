e8e1fbda051ee18a38178df75819d5e2e08436d7
AndrewHarris <aharris@setworks.com>
2022-08-10 11:10:40 -0500

    added nuke

---
+++ kcdcWorkshop/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("kcdcWorkshop.MainApplication, kcdcWorkshop, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc640182aaa2cd693ba0.MainApplication.class, crc640182aaa2cd693ba0.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
